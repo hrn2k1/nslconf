eefb4d3d7e86ab8270fcb04956c5db76c7452fcc
hrn2k1 <Harun.Rashid@dev.dctieto.fi>
2013-12-18 15:01:52 +0600

    n/a

--- mailee.js
+++ mailee.js
@@ -95,11 +95,11 @@ function checkConfMe(uri) {
 function checkMails() {
     /*console.log(imap);*/
     console.log('Connecting imap');
-    //imap.setMaxListeners(0);
+    imap.setMaxListeners(0);
      console.log('Connecting imap...');
-    imap.once('error', function(err) {
+   /* imap.once('error', function(err) {
   console.log(err);
-    });
+    });*/
     imap.connect(function(err) {
         if (err) {
             PARSE_RES['fetchMessage'] = 'Unable to connect imap: ' + err;
